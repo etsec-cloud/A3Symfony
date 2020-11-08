28335807e65527d111beb43dc59dac32056d4e27
BenjaminPichon <55100574+BenjaminPichon@users.noreply.github.com>
2020-11-08 14:03:23 +0100

    add user and tableau de bord

--- migrations/Version20201108125246.php
+++ migrations/Version20201108125246.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201108104005 extends AbstractMigration
+final class Version20201108125246 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,16 +20,12 @@ final class Version20201108104005 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipe ADD slug VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE joueur ADD slug VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE tournois ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipe DROP slug');
-        $this->addSql('ALTER TABLE joueur DROP slug');
-        $this->addSql('ALTER TABLE tournois DROP slug');
+        $this->addSql('DROP TABLE user');
     }
 }
