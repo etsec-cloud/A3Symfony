1ed04eee6d8c0ac4e180b114681acc6b31923870
BenjaminPichon <55100574+BenjaminPichon@users.noreply.github.com>
2020-11-08 17:40:00 +0100

    start equipe form (pour etienne)

--- src/Form/EquipeType.php
+++ src/Form/EquipeType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Equipe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class EquipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+            ->add('nom',TextType::class, [
+                'attr' => ['class'=> 'w-1/2']
+            ])
             ->add('Ajouter', SubmitType::class)
         ;
     }
